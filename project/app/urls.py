51ed09da81ed45a1cb0c13b830af4e77d1cd53b8
Muteeb Bhat <muteebhat7@gmail.com>
2024-01-30 23:37:32 +0530

    - create task view  created - get all tasks view created

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-
+    path('tasks',create_task, name='task_create'),
+    path('tasks/all', all_tasks, name='all_tasks'),
     # other endpoints...
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Task
+from .serializers import TaskSerializer
 # Create your views here.
+
+
+@api_view(['POST'])
+def create_task(request):
+    if request.data:
+        # Ensure 'completed' field is set to False by default
+        request.data['is_completed'] = False
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            return Response({'id': task.id}, status=status.HTTP_201_CREATED)
+        
+        
+@api_view(['GET'])
+def all_tasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response({"tasks" : serializer.data}, status=status.HTTP_200_OK)
