9ea46c3cfe29ff9c7293dbeb656056632d03b13e
Muteeb Bhat <muteebhat7@gmail.com>
2024-01-31 00:44:53 +0530

    -get , delete and edit specified task view created (TaskDetail)

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,5 +6,7 @@ from .views import *
 urlpatterns = [
     path('tasks',create_task, name='task_create'),
     path('tasks/all', all_tasks, name='all_tasks'),
+    path('tasks/<int:id>', TaskDetail.as_view(), name='task_detail'),
+
     # other endpoints...
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.views import APIView
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
@@ -23,3 +25,41 @@ def all_tasks(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response({"tasks" : serializer.data}, status=status.HTTP_200_OK)
+
+
+
+class TaskDetail(APIView):
+    def get_object(self, id):
+        try:
+            return Task.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return None
+
+    def get(self, request, id):
+        task = self.get_object(id)
+        if not task:
+            return Response({"error": "Task with id {} does not exist".format(id)}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        task = self.get_object(id)
+        if not task:
+            return Response({"error": "Task with id {} does not exist".format(id)}, status=status.HTTP_404_NOT_FOUND)
+
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+   
+
+    def put(self, request, id):
+        task = self.get_object(id)
+        if not task:
+            return Response({"error": "Task with id {} does not exist".format(id)}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
